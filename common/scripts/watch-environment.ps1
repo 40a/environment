edc978aae91300783cc372dffdf41bff4e69f3ae
Oleg Soroka <oleg40a@gmail.com>
2016-02-27 00:14:35 +0200

    changed output

--- common/scripts/watch-environment.ps1
+++ common/scripts/watch-environment.ps1
@@ -10,7 +10,7 @@ else
 { 
     iex (irm  "http://localhost:8500/v1/kv/infra/skynet/bootstrap?raw&token=$env:CONSUL_TOKEN") 
     $uri = gc c:\scripts\slack.txt
-    $msg = ">>> $(Get-Date) : $env:COMPUTERNAME : $(irm http://169.254.169.254/latest/meta-data/instance-id) : $($diff.tostring("#.#")) sec since last update"
+    $msg = ">>> $(Get-Date) : $env:COMPUTERNAME : $(irm http://169.254.169.254/latest/meta-data/instance-id) : $($diff.tostring("#.#")) sec since last update, $c changes"
     if ($uri ) { irm -Body $msg -Method post -Uri $uri }
     #Remove-Item c:\scripts\watch-lock.txt -Force
     (Get-Date).Ticks | Out-File -LiteralPath C:\Scripts\watch-last.txt -Force
