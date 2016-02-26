ccd86d668dbfff268691c66c65db6932b0c097cb
Oleg Soroka <oleg40a@gmail.com>
2016-02-27 00:02:13 +0200

    refactored detection

--- common/scripts/watch-environment.ps1
+++ common/scripts/watch-environment.ps1
@@ -3,14 +3,14 @@ $c = $i.Count
 "$c items read"
 #if (Test-Path c:\scripts\watch-lock.txt) {return 0}
 #Get-Date | Out-File c:\scripts\watch-lock.txt -Force
-$diff = (Get-Date).Ticks - [int64](gc C:\Scripts\watch-last.txt -ea SilentlyContinue)
-if ($c -eq 0 -or $diff -lt 30000000)
+$diff = ( (Get-Date).Ticks - [int64](gc C:\Scripts\watch-last.txt -ea SilentlyContinue) ) / 10000000
+if ($c -eq 0 -or $diff -lt 5)
 { "Void" }
 else
 { 
     iex (irm  "http://localhost:8500/v1/kv/infra/skynet/bootstrap?raw&token=$env:CONSUL_TOKEN") 
     $uri = gc c:\scripts\slack.txt
-    $msg = ">>> $(Get-Date) : $env:COMPUTERNAME : $(irm http://169.254.169.254/latest/meta-data/hostname) : $diff"
+    $msg = ">>> $(Get-Date) : $env:COMPUTERNAME : $(irm http://169.254.169.254/latest/meta-data/hostname) : $diff sec. since last update"
     if ($uri ) { irm -Body $msg -Method post -Uri $uri }
     #Remove-Item c:\scripts\watch-lock.txt -Force
     (Get-Date).Ticks | Out-File -LiteralPath C:\Scripts\watch-last.txt -Force
