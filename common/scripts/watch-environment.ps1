3a7acf54ccebdbf67ae1f637433a1a6c49ef9d2f
Oleg Soroka <oleg40a@gmail.com>
2016-02-26 21:35:23 +0200

    test 123

--- common/scripts/watch-environment.ps1
+++ common/scripts/watch-environment.ps1
@@ -7,9 +7,10 @@ if ($c -eq 0)
 { "Void" }
 else
 { 
-	iex (irm  "http://localhost:8500/v1/kv/infra/skynet/bootstrap?raw&token=$env:CONSUL_TOKEN") 
+    iex (irm  "http://localhost:8500/v1/kv/infra/skynet/bootstrap?raw&token=$env:CONSUL_TOKEN") 
 }
 
 $uri = gc c:\scripts\slack.txt
 $msg = "$env:COMPUTERNAME : $(irm http://169.254.169.254/latest/meta-data/hostname)"
 if ($uri ) { irm -Body $msg -Method post -Uri $uri }
+return 0
